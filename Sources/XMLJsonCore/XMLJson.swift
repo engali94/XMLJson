f98881dcbb73443e5c6746f2d8bd85c98f67076d
Ali Ammar <ali94_200970@yahoo.com>
2020-04-28 22:37:38 +0300

    Added ArgumentParser

---
+++ Sources/XMLJsonCore/XMLJson.swift
@@ -0,0 +1,16 @@
+//
+//  XMLJson.swift
+//  XMLJsonCore
+//
+//  Created by Ali on 28.04.2020.
+//
+
+import ArgumentParser
+
+struct XMLJson: ParsableCommand {
+    
+    
+    func run() throws {
+        
+    }
+}
