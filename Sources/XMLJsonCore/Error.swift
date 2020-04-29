fce2a8830e30b604fd1dc4bec2a1059818b909d7
Ali Ammar <ali94_200970@yahoo.com>
2020-04-29 17:41:40 +0300

    Added extra --dir argument to refer to the base dir

---
+++ Sources/XMLJsonCore/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  XMLJsonCore
+//
+//  Created by Ali on 29.04.2020.
+//
+
+import Foundation
+
+enum XMLJsonError: Error {
+    case invlaidInput // please specify a correct path to xml files or directory
+    
+}
--- Sources/XMLJsonCore/XMLJson.swift
+++ Sources/XMLJsonCore/XMLJson.swift
@@ -7,10 +7,41 @@
 
 import ArgumentParser
 
-struct XMLJson: ParsableCommand {
+public struct  XMLJsonCommand: ParsableCommand {
     
+    public static var configuration = CommandConfiguration(commandName: "XMLJson",abstract: "Convert XML Format to JSON Format.")
     
-    func run() throws {
+    public init() { }
+
+    @Option(name: .shortAndLong, help: "Absolute path to a directory containing the xml files.")
+    private var dir: String
+    
+    @Option(name: .shortAndLong, parsing: .upToNextOption, help: "XML files to be parsed.")
+    var files: [String]
+    
+    @Option(name: .shortAndLong, help: "JSON file(s) output directory.")
+    private var output: String?
+    
+    @Flag(name: .shortAndLong, help: "Show extra logging for debugging purposes.")
+    var verbose: Bool
+    
+    @Flag(name: .shortAndLong, help: "Merge all converted XML files into one JSON file.")
+    var merge: Bool
+    
+    @Flag(name: .shortAndLong, help: "Convert all XML files present in the specifed directory.")
+    var all: Bool
+    
+    public func run() throws {
+        
+        guard !dir.isEmpty  else {
+            throw XMLJsonError.invlaidInput
+        }
+        let parser = Parser(files: [dir], output: output, merge: merge, all: all)
+        try parser.parse()
+        let path = Path(dir)
+        print(path.type)
         
     }
 }
+
+
